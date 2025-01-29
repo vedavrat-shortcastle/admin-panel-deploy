5a2eb382f7cc5f3ac01887bed27516a7f3c09682
Vedavrat <vedavrat@shortcastle.com>
2025-01-29 12:43:38 +0530

    feat: initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/require-default-props": "off",
+      "import/prefer-default-export": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+  ...compat.extends("next/core-web-vitals", "airbnb", "airbnb-typescript"),
+  prettier,
 ];
 
 export default eslintConfig;
